8b5da811e5b3cb700f224cc5e294c1bca86773a2
Daniel do Vale Abreu Teixeira <vaaledanieel@gmail.com>
2024-11-29 11:23:49 -0300

    Update index.js

--- aulateste/src/index.js
+++ aulateste/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ListaDeTarefas from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListaDeTarefas />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-reportWebVitals();
